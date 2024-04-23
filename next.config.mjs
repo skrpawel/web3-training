83280e4019abeb2b0f225b20383dba838df311bf
Paweł Skrobski <pawelskrobski@MacBook-Pro-test.local>
2024-04-23 16:54:06 +0200

    Add PWA initial config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// Importing the next-pwa package as a default import
+import nextPWA from "next-pwa";
 
-export default nextConfig;
+// Define the Next.js configuration
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
+
+// Configure next-pwa options
+const pwaOptions = {
+  dest: "public", // Destination directory for PWA files
+  disable: process.env.NODE_ENV === "development", // Disable in development
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+};
+
+// Export the Next.js configuration wrapped with the next-pwa function and options
+export default nextPWA(pwaOptions)(nextConfig);
